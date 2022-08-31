1d6f733412e9a19114a90c0de4d5238e7f4058a4
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2022-08-31 09:29:37 +0000

    Add API endpoint for subscribing to new youtube channels

--- API/main.py
+++ API/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import json
 import mysql.connector
+import re
 
+# api
 app = FastAPI()
 templates = Jinja2Templates(directory='jinja/')
+
+# database
+conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
+cursor = conn.cursor()
+
+# regex
+youtube_matcher = re.compile("https://www.youtube.com/channel/([^/]+)")
 
 
 @app.get("/")
@@ -20,3 +29,25 @@ def root(request: Request):
         for item in response:
             videos.append(json.loads(item[0]))
     return templates.TemplateResponse('subs.jinja', {'request': request, 'videos': videos})
+
+
+@app.get("/add/youtube")
+def add_channel(url, name, alt_name=None, status_code=201):
+    conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
+    cursor = conn.cursor()
+    valid_url = youtube_matcher.match(url)
+    if valid_url and name:
+        rss_url = f'https://www.youtube.com/feeds/videos.xml?channel_id={valid_url[1]}'
+        cursor.execute("select * from channels where url = %s;", (rss_url,))
+        duplicate = cursor.fetchall()
+        if duplicate:
+            return {"status": "Value already in database",
+                    "dupe": duplicate,
+                    status_code: 400}
+        else:
+            cursor.execute("insert into channels (url, channel_name, alt_name) values (%s, %s, %s)",
+                           (rss_url, name, alt_name))
+            conn.commit()
+            return {"status": "success", "url": url, "name": name, "alt_name": alt_name, }
+    else:
+        return {"status": "Invalid arguments"}
