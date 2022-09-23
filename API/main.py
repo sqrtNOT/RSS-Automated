2a0197dc399b93a811d549ec76dd566a6b5bd6f7
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2022-09-23 01:11:08 +0000

    cleanup database connections

--- API/main.py
+++ API/main.py
@@ -9,15 +9,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory='jinja/')
 
 # database
-conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
-cursor = conn.cursor()
-
+user='subsapi'
+password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k'
+host='localhost'
+database='rss'
 # regex
 youtube_matcher = re.compile("https://www.youtube.com/channel/([^/]+)")
 
 @app.get("/")
 def root(request: Request):
-    conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
+    conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
     cursor = conn.cursor()
     cursor.execute("select json_object('channel', channel_name, 'video', video_title, 'url', video_url) from videos join channels using (channel_id) where status is NULL order by video_date desc;")
     response = cursor.fetchall()
@@ -32,7 +33,7 @@ def root(request: Request):
 
 @app.get("/add/youtube")
 def add_channel(url, name, alt_name=None, status_code=201):
-    conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
+    conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
     cursor = conn.cursor()
     valid_url = youtube_matcher.match(url)
     if valid_url and name:
@@ -50,9 +51,11 @@ def add_channel(url, name, alt_name=None, status_code=201):
             return {"status": "success", "url": url, "name": name, "alt_name": alt_name, }
     else:
         return {"status": "Invalid arguments"}
+
+
 @app.get("/add")
 def add_rss(url, name, alt_name=None, status_code=201):
-    conn = mysql.connector.connect(user='subsapi', password='nLzM6KaoC50tOuKUPTvFl3WjIRYI4vac63vLwbpeCQvF5T6k', host='localhost', database='rss')
+    conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
     cursor = conn.cursor()
     if name and url:
         cursor.execute("select * from channels where url = %s;", (url,))
