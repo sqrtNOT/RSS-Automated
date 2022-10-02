ad2d54134f0ef82c47a112ee62b91a583a52faa3
sqrtNOT <77981959+sqrtNOT@users.noreply.github.com>
2022-10-02 07:46:33 +0000

    Add support for niconico rss feeds

--- API/main.py
+++ API/main.py
@@ -15,7 +15,7 @@ host = 'localhost'
 database = 'rss'
 # regex
 youtube_matcher = re.compile("https://www.youtube.com/channel/([^/]+)")
-
+nico_matcher = re.compile("https://www.nicovideo.jp/(user/\d+/video|mylist/\d+)")
 
 @app.get("/")
 def root(request: Request):
@@ -54,6 +54,28 @@ def add_channel(url, name, alt_name=None, status_code=201):
         return {"status": "Invalid arguments"}
 
 
+@app.get("/add/niconico")
+def add_channel(url, name, alt_name=None, status_code=201):
+    conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
+    cursor = conn.cursor()
+    valid_url = nico_matcher.match(url)
+    if valid_url and name:
+        rss_url = f'{valid_url[0]}?rss=2.0'
+        cursor.execute("select * from channels where url = %s;", (rss_url,))
+        duplicate = cursor.fetchall()
+        if duplicate:
+            return {"status": "Value already in database",
+                    "dupe": duplicate,
+                    status_code: 400}
+        else:
+            cursor.execute("insert into channels (url, channel_name, alt_name) values (%s, %s, %s)",
+                           (rss_url, name, alt_name))
+            conn.commit()
+            return {"status": "success", "url": url, "name": name, "alt_name": alt_name, }
+    else:
+        return {"status": "Invalid arguments"}
+
+
 @app.get("/add")
 def add_rss(url, name, alt_name=None, status_code=201):
     conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
@@ -72,3 +94,6 @@ def add_rss(url, name, alt_name=None, status_code=201):
             return {"status": "success", "url": url, "name": name, "alt_name": alt_name, }
     else:
         return {"status": "Invalid arguments"}
+
+
+
